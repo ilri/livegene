900d538e5846712993124bec4d2fb5af92866846
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-02-21 08:39:13 +0000

    Install DoctrineFixturesBundle

--- config/bundles.php
+++ config/bundles.php
@@ -29,4 +29,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Sonata\UserBundle\SonataUserBundle::class => ['all' => true],
     App\Application\Sonata\UserBundle\ApplicationSonataUserBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
