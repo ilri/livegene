d0dabbc36dffc52e1fc8741ca85b37c39588848d
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-02-25 13:47:50 +0000

    Add data fixtures for PartnershipType

---
+++ src/DataFixtures/PartnershipTypeFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\PartnershipType;
+
+class PartnershipTypeFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $descriptions = [
+            'Unspecified',
+            'Co-Investigator',
+            'Collaborator',
+            'Sub-contractor',
+            'Sub-contractee'
+        ];
+
+        foreach ($descriptions as $description) {
+            $partnershipType = new PartnershipType();
+            $partnershipType->setDescription($description);
+            $manager->persist($partnershipType);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['prod'];
+    }
+}
