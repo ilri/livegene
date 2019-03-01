1d3789b0ef80882480cd4c156c3b59eb06b043bd
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-03-01 15:30:41 +0000

    Add service RequestListener to set PRAGMA foreign_keys = 'ON'

---
+++ src/EventListener/RequestListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventListener;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class RequestListener
+{
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $this->doctrine->getConnection()->exec('PRAGMA foreign_keys = ON');
+    }
+}
