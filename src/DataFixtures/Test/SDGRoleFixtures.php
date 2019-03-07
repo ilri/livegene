5f4d4f4b837dc70b002a0cdf21ec50e2ed9f0d34
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-03-07 14:30:12 +0000

    API endpoint for SDGRole

---
+++ src/DataFixtures/Test/SDGRoleFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures\Test;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\SDGRole;
+use App\DataFixtures\SDGFixtures;
+
+class SDGRoleFixtures extends Fixture implements DependentFixtureInterface, FixtureGroupInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $sdgRole = new SDGRole();
+        $sdgRole->setProject($this->getReference('project'));
+        $sdgRole->setSDG($this->getReference('sdg1'));
+        $sdgRole->setPercent(50);
+        $manager->persist($sdgRole);
+
+        $manager->flush();
+
+        $this->addReference('sdg-role', $sdgRole);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProjectFixtures::class,
+            SDGFixtures::class,
+        ];
+    }
+
+    public static function getGroups(): array
+    {
+        return ['api'];
+    }
+}
---
+++ tests/API/SDGRoleAPITest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Tests\API;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use App\DataFixtures\Test\UserFixtures;
+
+class SDGRoleAPITest extends WebTestCase
+{
+    private $fixtures = null;
+    private $client;
+
+    public function setUp()
+    {
+        $this->fixtures = $this->loadFixtures([
+            'App\DataFixtures\Test\UserFixtures',
+            'App\DataFixtures\Test\SDGRoleFixtures',
+        ])->getReferenceRepository();
+        $username = $this->fixtures->getReference('user')->getUsername();
+        $credentials = [
+            'username' => $username,
+            'password' => UserFixtures::PASSWORD
+        ];
+        $this->client = $this->makeClient($credentials);
+    }
+
+    public function testGetCollectionIsAvailable()
+    {
+        $this->client->request('GET', '/api/sdg_roles', [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+        ]);
+        $this->assertSame(
+            Response::HTTP_OK,
+            $this->client->getResponse()->getStatusCode()
+        );
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json; charset=utf-8'
+            )
+        );
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertCount(1, $data);
+    }
+
+    public function testPostIsNotAllowed()
+    {
+        $this->client->request('POST', '/api/sdg_roles', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ]);
+        $this->assertSame(
+            Response::HTTP_METHOD_NOT_ALLOWED,
+            $this->client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testGetItemIsAvailable()
+    {
+        $sdgRole = $this->getSDGRole();
+        $this->client->request('GET', sprintf('/api/sdg_roles/%s', $sdgRole), [], [], [
+            'HTTP_ACCEPT' => 'application/json'
+        ]);
+        $this->assertSame(
+            Response::HTTP_OK,
+            $this->client->getResponse()->getStatusCode()
+        );
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json; charset=utf-8'
+            )
+        );
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('project', $data);
+        $this->assertArrayHasKey('SDG', $data);
+        $this->assertArrayHasKey('percent', $data);
+        $this->assertSame(
+            $data,
+            [
+                'id' => 1,
+                'project' => '/api/projects/1',
+                'SDG' => '/api/sdgs/1',
+                'percent' => 50,
+            ]
+        );
+    }
+
+    public function testPutIsNotAllowed()
+    {
+        $sdgRole = $this->getSDGRole();
+        $this->client->request('PUT', sprintf('/api/sdg_roles/%s', $sdgRole), [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ]);
+        $this->assertSame(
+            Response::HTTP_METHOD_NOT_ALLOWED,
+            $this->client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testDeleteIsNotAllowed()
+    {
+        $sdgRole = $this->getSDGRole();
+        $this->client->request('DELETE', sprintf('/api/sdg_roles/%s', $sdgRole), [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ]);
+        $this->assertSame(
+            Response::HTTP_METHOD_NOT_ALLOWED,
+            $this->client->getResponse()->getStatusCode()
+        );
+    }
+
+    private function getSDGRole()
+    {
+        return $this->fixtures->getReference('sdg-role')->getId();
+    }
+}
