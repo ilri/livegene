55ef611e9044c8252a098bc635abaddb47e1080a
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-04-08 11:36:10 +0100

    Add deploy config

---
+++ config/prod/deploy.php
@@ -0,0 +1,33 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
+            ->server('ovh')
+            // the absolute path of the remote server directory where the project is deployed
+            ->deployDir('/home/liveged/www')
+            // the URL of the Git repository where the project code is hosted
+            ->repositoryUrl('git@github.com:cezar77/livegene_backend.git')
+            // the repository branch to deploy
+            ->repositoryBranch('master')
+        ;
+    }
+
+    // run some local or remote commands before the deployment is started
+    public function beforeStartingDeploy()
+    {
+        // $this->runLocal('./vendor/bin/simple-phpunit');
+    }
+
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+        // $this->runRemote('{{ console_bin }} app:my-task-name');
+        // $this->runLocal('say "The deployment has finished."');
+    }
+};
