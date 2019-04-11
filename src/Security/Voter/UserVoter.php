7e0815a98970245387ec3bb8ed6daf51a4df5724
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-04-11 12:22:00 +0100

    Add UserVoter

---
+++ src/Security/Voter/UserVoter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use App\Application\Sonata\UserBundle\Entity\User;
+
+class UserVoter extends Voter
+{
+    // these strings are just invented: you can use anything
+    const VIEW = 'ROLE_SONATA_USER_ADMIN_USER_VIEW';
+    const EDIT = 'ROLE_SONATA_USER_ADMIN_USER_EDIT';
+
+    protected function supports($attribute, $subject)
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, [self::VIEW, self::EDIT])) {
+            return false;
+        }
+
+        if (!$subject instanceof User) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return $this->canView($subject, $user);
+            case self::EDIT:
+                return $this->canEdit($subject, $user);
+        }
+
+        throw new \LogicException('This code should not be reached!');
+    }
+
+    private function canView(User $subject, User $user)
+    {
+        // if they can edit, they can view
+        if ($this->canEdit($subject, $user)) {
+            return true;
+        }
+    }
+
+    private function canEdit(User $subject, User $user)
+    {
+        return $user === $subject;
+    }
+}
