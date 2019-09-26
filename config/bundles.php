cd933d815a66fb15ed1d98f177a461f9ea653cf3
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-09-26 13:21:32 +0100

    Install LexikJWTAuthenticationBundle

--- config/bundles.php
+++ config/bundles.php
@@ -33,4 +33,5 @@ return [
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
