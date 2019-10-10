e69fd843e9b42e8ca6ff43083472072811eabfdc
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-10-10 08:46:49 +0100

    Install ckeditor

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,14 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+    .copyFiles([
+      {from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+      {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+      {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+      {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+      {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+    ])
+    .addLoader({test: /\.json$/i, include: [require('path').resolve(__dirname, 'node_modules/ckeditor')], loader: 'raw-loader', type: 'javascript/auto'})
 ;
 
 module.exports = Encore.getWebpackConfig();
