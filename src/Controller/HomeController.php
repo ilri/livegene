d719230c281eef549be7a433cecfd0c3e90e7502
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-10-16 14:37:47 +0100

    Programmatically login user in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Application\Sonata\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class HomeController extends AbstractController
 {
@@ -12,6 +14,19 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+        $username = $this->getParameter('spa_user');
+        $user = $userRepository->findOneByUsername($username);
+
+        $token = new UsernamePasswordToken(
+            $user,
+            null,
+            'main',
+            $user->getRoles()
+        );
+        $this->container->get('security.token_storage')->setToken($token);
+        $this->container->get('session')->set('_security_main', serialize($token));
+
         return $this->render('home/index.html.twig');
     }
 }
