bf8036ffde56080086700092d2a7dd84d9a70afb
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2020-12-19 14:55:46 +0000

    Create ProjectService and use it in store module project

--- assets/spa/services/ProjectService.js
+++ assets/spa/services/ProjectService.js
@@ -1,5 +1,23 @@
+import axios from 'axios';
+
 export default {
-  getProjects(url) {
-    axios.get(url);
+  /**
+   * Get all projects from the endpoint /api/projects
+   * @param url
+   * @param config
+   * @param data
+   * @returns {Promise<*[]>}
+   */
+  async getProjects(url, config, data = []) {
+    await axios.get(url, config)
+      .then((response) => {
+        data.push(...response.data['hydra:member']);
+        if (response.data['hydra:view'] && response.data['hydra:view']['hydra:next']) {
+          return this.getProjects(response.data['hydra:view']['hydra:next'], config, data);
+        }
+        return data;
+      })
+    ;
+    return data;
   },
 };
--- assets/spa/store/modules/project.js
+++ assets/spa/store/modules/project.js
@@ -1,5 +1,6 @@
 import { ascending } from 'd3';
 import { group } from 'd3-array';
+import ProjectService from '../../services/ProjectService';
 
 const d3 = {
   ascending,
@@ -21,55 +22,41 @@ export default {
     },
   },
   actions: {
-    async getProjectsAction(context) {
-      let url = '/api/projects';
+    getProjectsAction(context) {
+      const url = '/api/projects';
+      const config = {
+        params: {
+          properties: [
+            'id',
+            'ilriCode',
+            'fullName',
+            'shortName',
+            'team',
+            'principalInvestigator',
+            'startDate',
+            'endDate',
+            'donor',
+            'totalProjectValue',
+            'partnerships',
+            'staffRoles',
+            'countryRoles',
+            'isActive',
+            'isActiveThisYear',
+          ],
+        },
+      };
 
-      try {
-        while (url) {
-          /* eslint-disable no-await-in-loop */
-          const response = await context.rootGetters.authenticatedAxios.get(
-            url,
-            {
-              params: {
-                properties: [
-                  'id',
-                  'ilriCode',
-                  'fullName',
-                  'shortName',
-                  'team',
-                  'principalInvestigator',
-                  'startDate',
-                  'endDate',
-                  'donor',
-                  'totalProjectValue',
-                  'partnerships',
-                  'staffRoles',
-                  'countryRoles',
-                  'isActive',
-                  'isActiveThisYear',
-                ],
-              },
-            },
-          );
-          /* eslint-enable no-await-in-loop */
-          context.commit(
-            'SET_PROJECTS',
-            response.data['hydra:member'],
-          );
-          if (response.data['hydra:view']) {
-            url = response.data['hydra:view']['hydra:next'];
-          } else {
-            url = null;
-          }
-        }
-      } catch (error) {
-        context.commit('setError', error.response.statusText);
-        console.error(error);
-        return;
-      }
-
-      context.commit('setLoaded');
-      context.commit('SORT_AND_GROUP_PROJECTS');
+      ProjectService.getProjects(url, config)
+        .then((response) => {
+          context.commit('SET_PROJECTS', response);
+          context.commit('setLoaded');
+          context.commit('SORT_AND_GROUP_PROJECTS');
+        })
+        .catch((error) => {
+          context.commit('setError', error.response.statusText);
+          console.error(error);
+        })
+      ;
     },
   },
 };
