9760383d5ea2aa2f28cf138099edde5faca578d0
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2020-12-26 19:44:27 +0000

    Clean up auth module in Vuex store

--- assets/spa/store/modules/auth.js
+++ assets/spa/store/modules/auth.js
@@ -3,27 +3,11 @@ import Cookies from 'js-cookie';
 
 export default {
   state: {
-    authenticated: false,
     jwt: null,
-  },
-  getters: {
-    authenticatedAxios(state) {
-      return axios.create({
-        method: 'get',
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest',
-          Accept: 'application/ld+json',
-          Authorization: `Bearer ${state.jwt}`,
-        },
-      });
-    },
   },
   mutations: {
     SET_JWT(state, cookie) {
       state.jwt = cookie;
-    },
-    setAuthenticated(state) {
-      state.authenticated = true;
     },
   },
   actions: {
@@ -41,29 +25,6 @@ export default {
       axios.defaults.headers.get['Accept'] = 'application/ld+json';
       axios.defaults.headers.common['Authorization'] = `Bearer ${context.state.jwt}`;
       /* eslint-enable dot-notation */
-    },
-    async authenticate(context, state) {
-      try {
-        const response = await axios({
-          url: '/api',
-          method: 'post',
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            Authorization: `Bearer ${state.jwt}`,
-          },
-        });
-
-        if (response.status === 200) {
-          context.commit('setAuthenticated');
-        }
-      } catch (err) {
-        if (err.response.status === 401) {
-          console.log(err);
-          throw new Error('No JSON Web Token found.');
-        } else {
-          throw new Error('Login failed. Please try later or contact the administrator.');
-        }
-      }
     },
   },
 };
