f224faaf426ec919723e8dd56380bea21369d848
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2021-01-12 10:57:03 +0000

    Enhance ErrorBar

--- assets/spa/components/ErrorBar.vue
+++ assets/spa/components/ErrorBar.vue
@@ -1,19 +1,24 @@
 <template>
-  <b-row>
-    <b-alert
-      v-for="error in errors"
-      :key="error.id"
-      show
-      variant="warning"
-      class="mx-auto col-9 text-center"
+  <b-row class="mx-0">
+    <b-col
+      cols="12"
+      lg="8"
+      class="mx-auto text-center"
     >
-      <h4 class="alert-heading">
-        ERROR
-      </h4>
-      <hr>
-      <p>{{ error.message }}</p>
-      <p>{{ error.status }} - {{ error.statusText }}</p>
-    </b-alert>
+      <b-alert
+        v-for="error in errors"
+        :key="error.id"
+        show
+        variant="warning"
+      >
+        <h4 class="alert-heading">
+          ERROR
+        </h4>
+        <hr>
+        <p>{{ error.message }}</p>
+        <p>{{ error.status }} - {{ error.statusText }}</p>
+      </b-alert>
+    </b-col>
   </b-row>
 </template>
 
