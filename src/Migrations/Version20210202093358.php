3cfc96618254d8ae03b01902e9760732b780e055
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2021-02-02 09:34:42 +0000

    Add startDate and endDate to StaffRole

---
+++ src/Migrations/Version20210202093358.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210202093358 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE app_staff_role ADD start_date DATE DEFAULT NULL, ADD end_date DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE app_staff_role_audit ADD start_date DATE DEFAULT NULL, ADD end_date DATE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE app_staff_role DROP start_date, DROP end_date');
+        $this->addSql('ALTER TABLE app_staff_role_audit DROP start_date, DROP end_date');
+    }
+}
