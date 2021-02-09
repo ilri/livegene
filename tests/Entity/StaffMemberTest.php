5724fdd58bed235bac4b1e0777973823376fe43a
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2021-02-09 06:28:54 +0000

    Refactor unit tests for StaffMember

--- tests/Entity/StaffMemberTest.php
+++ tests/Entity/StaffMemberTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Entity;
 
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
@@ -13,15 +14,45 @@ class StaffMemberTest extends TestCase
     private static $email = 'Cezar.Pendarovski@ROSLIN.ed.ac.uk';
     private static $username = 'Cpendaro';
     private static $staffMember;
+    private static $staffRole1;
+    private static $staffRole2;
+    private static $staffRole3;
+    private static $totalPercent;
 
     public static function setUpBeforeClass()
     {
+        date_default_timezone_set('UTC');
+        $now = Carbon::create(2021, 2, 1, 12);
+        Carbon::setTestNow($now);
+
         self::$staffMember = new StaffMember();
         self::$staffMember->setUsername(self::$username);
         self::$staffMember->setFirstName('Cezar');
         self::$staffMember->setLastName('Pendarovski');
         self::$staffMember->setHomeProgram('CTLGH');
         self::$staffMember->setEmail(self::$email);
+
+        $percent1 = 0.2500;
+        $percent2 = 0.2750;
+        $percent3 = 0.4750;
+        self::$totalPercent = $percent2 + $percent3;
+
+        self::$staffRole1 = new StaffRole();
+        self::$staffRole1->setPercent($percent1);
+        self::$staffRole1->setStartDate(new \DateTime('2020-01-01'));
+        self::$staffRole1->setEndDate(new \DateTime('2020-12-31'));
+        self::$staffRole2 = new StaffRole();
+        self::$staffRole2->setPercent($percent2);
+        self::$staffRole2->setStartDate(new \DateTime('2021-01-01'));
+        self::$staffRole2->setEndDate(new \DateTime('2021-12-31'));
+        self::$staffRole3 = new StaffRole();
+        self::$staffRole3->setPercent($percent3);
+        self::$staffRole3->setStartDate(new \DateTime('2020-01-01'));
+        self::$staffRole3->setEndDate(new \DateTime('2021-12-31'));
+
+        self::$staffMember->addStaffRole(self::$staffRole1);
+        self::$staffMember->addStaffRole(self::$staffRole2);
+        self::$staffMember->addStaffRole(self::$staffRole3);
     }
 
     public function setUp()
@@ -49,30 +80,30 @@ class StaffMemberTest extends TestCase
         $this->assertNotEquals(self::$email, self::$staffMember->getEmail());
     }
 
+    public function testGetActiveStaffRoles()
+    {
+        $this->assertEquals(
+            2,
+            sizeof(self::$staffMember->getActiveStaffRoles())
+        );
+        $this->assertNotContains(
+            self::$staffRole1,
+            self::$staffMember->getActiveStaffRoles()
+        );
+        $this->assertContains(
+            self::$staffRole2,
+            self::$staffMember->getActiveStaffRoles()
+        );
+        $this->assertContains(
+            self::$staffRole3,
+            self::$staffMember->getActiveStaffRoles()
+        );
+    }
+
     public function testTotalRolesPercent()
     {
         $this->assertEquals(
-            0,
-            self::$staffMember->getTotalStaffRolesPercent()
-        );
-
-        $percent1 = 0.2500;
-        $percent2 = 0.2750;
-        $percent3 = 0.4750;
-        $staffRole1 = new StaffRole();
-        $staffRole1->setPercent($percent1);
-        $staffRole2 = new StaffRole();
-        $staffRole2->setPercent($percent2);
-        $staffRole3 = new StaffRole();
-        $staffRole3->setPercent($percent3);
-        $totalPercent = $percent1 + $percent2 + $percent3;
-
-        self::$staffMember->addStaffRole($staffRole1);
-        self::$staffMember->addStaffRole($staffRole2);
-        self::$staffMember->addStaffRole($staffRole3);
-
-        $this->assertEquals(
-            $totalPercent,
+            self::$totalPercent,
             self::$staffMember->getTotalStaffRolesPercent()
         );
     }
