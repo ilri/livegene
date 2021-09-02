3acf4450b2c5ff2e2c1fc5b4636fbc65ce77b793
Cezar Pendarovski <cezarpendarovski@web.de>
2021-09-02 07:09:17 +0100

    Add SonataExporterBundle in bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -39,4 +39,5 @@ return [
     Sonata\Doctrine\Bridge\Symfony\SonataDoctrineBundle::class => ['all' => true],
     Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
     Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterBundle::class => ['all' => true],
 ];
