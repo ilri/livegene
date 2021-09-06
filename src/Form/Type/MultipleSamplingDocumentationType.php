fe98abc8a369dec13c7d37c14fa85d3127e0caf1
Johanna Benndorf <johanna.benndorf@gmail.com>
2021-09-06 13:17:41 +0200

    Refactor src/Form files

--- src/Form/Type/MultipleSamplingDocumentationType.php
+++ src/Form/Type/MultipleSamplingDocumentationType.php
@@ -2,22 +2,21 @@
 
 namespace App\Form\Type;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Sonata\AdminBundle\Form\Type\ModelType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use App\Entity\{
     SamplingActivity,
     SamplingDocumentType,
-    SamplingDocumentation
 };
+use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MultipleSamplingDocumentationType extends AbstractType
 {
-    private $container;
+    private ContainerInterface $container;
 
     public function __construct(ContainerInterface $container)
     {
@@ -59,7 +58,7 @@ class MultipleSamplingDocumentationType extends AbstractType
             ])
         ;
     }
-    
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
