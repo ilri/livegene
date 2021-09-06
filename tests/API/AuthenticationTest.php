1567d58725f54b856d44608002af155d8e946aef
Cezar Pendarovski <cezarpendarovski@web.de>
2021-09-06 07:44:50 +0100

    Create AuthenticationTest class

---
+++ tests/API/AuthenticationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\API;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\DataFixtures\Test\UserFixtures;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+
+class AuthenticationTest extends ApiTestCase
+{
+    public function testLogin()
+    {
+        $client = self::createClient();
+
+        $databaseTool = $client->getContainer()->get(DatabaseToolCollection::class)->get();
+        $fixtures = $databaseTool->loadFixtures([
+            'App\DataFixtures\Test\UserFixtures',
+        ])->getReferenceRepository();
+
+
+        $response = $client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'username' => $fixtures->getReference('api_user')->getUsername(),
+                'password' => UserFixtures::PASSWORD,
+            ],
+        ]);
+
+        $json = $response->toArray();
+        $this->assertResponseIsSuccessful();
+        $this->assertArrayHasKey('token', $json);
+    }
+}
