8723fffa7721254a42067e79a0ff5fc0c8f4bae2
Johanna Benndorf <johanna.benndorf@gmail.com>
2021-09-07 22:33:12 +0200

    Refactor API test for SDGRole

---
+++ tests/API/SDGRoleAPITest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Tests\API;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\{
+    ApiTestCase,
+    Client,
+};
+use App\DataFixtures\Test\UserFixtures;
+use App\Entity\SDGRole;
+use Doctrine\Common\DataFixtures\ReferenceRepository;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Symfony\Component\HttpFoundation\Response;
+
+class SDGRoleAPITest extends ApiTestCase
+{
+    private Client $client;
+    private ReferenceRepository $fixtures;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $databaseTool = $this->client->getContainer()->get(DatabaseToolCollection::class)->get();
+        $this->fixtures = $databaseTool->loadFixtures([
+            'App\DataFixtures\Test\UserFixtures',
+            'App\DataFixtures\Test\SDGRoleFixtures',
+        ])->getReferenceRepository();
+        $username = $this->fixtures->getReference('api_user')->getUsername();
+        $credentials = [
+            'username' => $username,
+            'password' => UserFixtures::PASSWORD
+        ];
+        $response = $this->client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => $credentials,
+        ]);
+        $this->client->setDefaultOptions([
+            'auth_bearer' => json_decode($response->getContent(), true)['token'],
+        ]);
+    }
+
+    public function testGetCollectionIsAvailable(): void
+    {
+        $response = $this->client->request('GET', '/api/sdg_roles');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/SDGRole',
+            '@id' => '/api/sdg_roles',
+            '@type' => 'hydra:Collection',
+            'hydra:member' => [
+                [
+                    'id' => 1,
+                    'project' => '/api/projects/1',
+                    'SDG' => '/api/sdgs/1',
+                    'percent' => '0.5',
+                ]
+            ],
+            'hydra:totalItems' => 1,
+        ]);
+        $this->assertCount(1, $response->toArray()['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(SDGRole::class);
+    }
+
+    public function testPostIsNotAllowed(): void
+    {
+        $this->client->request('POST', '/api/sdg_roles');
+        $this->assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    public function testGetItemIsAvailable(): void
+    {
+        $sdgRole = $this->getSDGRole();
+        $this->client->request('GET', sprintf('/api/sdg_roles/%s', $sdgRole));
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+                'id' => 1,
+                'project' => '/api/projects/1',
+                'SDG' => '/api/sdgs/1',
+                'percent' => '0.5',
+            ]
+        );
+    }
+
+    public function testPutIsNotAllowed(): void
+    {
+        $sdgRole = $this->getSDGRole();
+        $this->client->request('PUT', sprintf('/api/sdg_roles/%s', $sdgRole));
+        $this->assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    public function testDeleteIsNotAllowed(): void
+    {
+        $sdgRole = $this->getSDGRole();
+        $this->client->request('DELETE', sprintf('/api/sdg_roles/%s', $sdgRole));
+        $this->assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    private function getSDGRole(): int
+    {
+        return $this->fixtures->getReference('sdg-role')->getId();
+    }
+}
