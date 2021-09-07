686cb8be53616a9961f0a828c349c2acbed89660
Johanna Benndorf <johanna.benndorf@gmail.com>
2021-09-07 15:40:54 +0200

    Refactor API test for Organisation

---
+++ tests/API/OrganisationAPITest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Tests\API;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\{
+    ApiTestCase,
+    Client,
+};
+use App\Entity\Organisation;
+use Doctrine\Common\DataFixtures\ReferenceRepository;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Symfony\Component\HttpFoundation\Response;
+use App\DataFixtures\Test\UserFixtures;
+
+class OrganisationAPITest extends ApiTestCase
+{
+    private Client $client;
+    private ReferenceRepository $fixtures;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $databaseTool = $this->client->getContainer()->get(DatabaseToolCollection::class)->get();
+        $this->fixtures = $databaseTool->loadFixtures([
+            'App\DataFixtures\Test\UserFixtures',
+            'App\DataFixtures\Test\OrganisationFixtures',
+        ])->getReferenceRepository();
+        $username = $this->fixtures->getReference('api_user')->getUsername();
+        $credentials = [
+            'username' => $username,
+            'password' => UserFixtures::PASSWORD
+        ];
+        $response = $this->client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => $credentials,
+        ]);
+        $this->client->setDefaultOptions([
+            'auth_bearer' => json_decode($response->getContent(), true)['token'],
+        ]);
+    }
+
+    public function testGetCollectionIsAvailable(): void
+    {
+        $response = $this->client->request('GET', '/api/organisations');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Organisation',
+            '@id' => '/api/organisations',
+            '@type' => 'hydra:Collection',
+            'hydra:member' => [
+                [
+                    'id' => 1,
+                    'shortName' => 'ACME',
+                    'fullName' => 'A Company Making Everything',
+                    'localName' => 'A Company Making Everything',
+                    'link' => 'https://www.acme.co.uk/',
+                    'logoUrl' => 'https://www.acme.co.uk/images/logo.png',
+                    'country' => [
+                        'id' => 1,
+                        'country' => 'GB',
+                        'countryName' => 'United Kingdom'
+                    ]
+                ]
+            ],
+            'hydra:totalItems' => 1,
+        ]);
+        $this->assertCount(1, $response->toArray()['hydra:member']);
+        // need to check why this is not working
+        //$this->assertMatchesResourceCollectionJsonSchema(Organisation::class);
+    }
+
+    public function testPostIsNotAllowed(): void
+    {
+        $this->client->request('POST', '/api/organisations');
+        $this->assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    public function testGetItemIsAvailable(): void
+    {
+        $organisation = $this->getOrganisation();
+        $this->client->request('GET', sprintf('/api/organisations/%s', $organisation));
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+                'id' => 1,
+                'shortName' => 'ACME',
+                'fullName' => 'A Company Making Everything',
+                'localName' => 'A Company Making Everything',
+                'link' => 'https://www.acme.co.uk/',
+                'logoUrl' => 'https://www.acme.co.uk/images/logo.png',
+                'country' => [
+                    'id' => 1,
+                    'country' => 'GB',
+                    'countryName' => 'United Kingdom'
+                ]
+            ]
+        );
+    }
+
+    public function testPutIsNotAllowed(): void
+    {
+        $organisation = $this->getOrganisation();
+        $this->client->request('PUT', sprintf('/api/organisations/%s', $organisation));
+        $this->assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    public function testDeleteIsNotAllowed(): void
+    {
+        $organisation = $this->getOrganisation();
+        $this->client->request('DELETE', sprintf('/api/organisations/%s', $organisation));
+        $this->assertResponseStatusCodeSame(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    private function getOrganisation(): int
+    {
+        return $this->fixtures->getReference('organisation')->getId();
+    }
+}
