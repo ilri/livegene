bd880457fa4d60f3c28269b72da4dde5c97672c0
Cezar Pendarovski <cezarpendarovski@web.de>
2021-09-09 10:23:11 +0100

    Adjust code style in App\OpenApi

--- src/OpenApi/JwtDecorator.php
+++ src/OpenApi/JwtDecorator.php
@@ -5,18 +5,27 @@ declare(strict_types=1);
 namespace App\OpenApi;
 
 use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
-use ApiPlatform\Core\OpenApi\OpenApi;
-use ApiPlatform\Core\OpenApi\Model;
+use ApiPlatform\Core\OpenApi\{
+    OpenApi,
+    Model,
+};
 
 final class JwtDecorator implements OpenApiFactoryInterface
 {
-    private $decorated;
+    private OpenApiFactoryInterface $decorated;
 
+    /**
+     * @param OpenApiFactoryInterface $decorated
+     */
     public function __construct(OpenApiFactoryInterface $decorated)
     {
         $this->decorated = $decorated;
     }
 
+    /**
+     * @param array $context
+     * @return OpenApi
+     */
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = ($this->decorated)($context);
@@ -34,13 +43,13 @@ final class JwtDecorator implements OpenApiFactoryInterface
         $schemas['Credentials'] = new \ArrayObject([
             'type' => 'object',
             'properties' => [
-                'email' => [
+                'username' => [
                     'type' => 'string',
-                    'example' => 'johndoe@example.com',
+                    'example' => 'username',
                 ],
                 'password' => [
                     'type' => 'string',
-                    'example' => 'apassword',
+                    'example' => 'password',
                 ],
             ],
         ]);
