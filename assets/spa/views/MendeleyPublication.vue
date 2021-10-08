34bd928622dcbcd5dfe4a4e12694c51e5d0ad022
Cezar Pendarovski <cezarpendarovski@web.de>
2021-10-08 13:09:52 +0100

    Draft for displaying details of Mendeley publication

--- assets/spa/views/MendeleyPublication.vue
+++ assets/spa/views/MendeleyPublication.vue
@@ -11,22 +11,72 @@
     <template slot="graphic">
       <b-col
         cols="12"
-        lg="8"
+        lg="10"
         class="px-0"
       >
-        <b-card no-body>
-          <b-card-body>
-            <b-card-title>{{ publication.title }}</b-card-title>
-            <b-card-sub-title>{{ authors }}</b-card-sub-title>
-            <b-card-text>
-              <p class="alert alert-info">
-                {{ publication.type }}
-              </p>
-              <p>{{ added }}</p>
-            </b-card-text>
-            <b-card-text>{{ publication.abstract }}</b-card-text>
-          </b-card-body>
-        </b-card>
+        <div class="border border-primary rounded">
+          <h4 class="bg-info text-light py-3">
+            {{ publication.title }}
+          </h4>
+          <p class="h5 text-muted">
+            {{ authors }}
+          </p>
+          <p class="badge badge-info badge-pill p-2">
+            {{ type }}
+          </p>
+          <p>
+            {{ publication.source }}, {{ publication.year }}
+          </p>
+          <b-col
+            cols="4"
+            offset="4"
+          >
+            <b-table-simple
+              v-if="publication.identifiers"
+              hover
+              small
+              caption-top
+              responsive
+            >
+              <caption>Identifiers:</caption>
+              <b-tr
+                v-for="(value, key) in publication.identifiers"
+                :key="key"
+              >
+                <b-th>{{ key.toUpperCase() }}</b-th>
+                <b-td>{{ value }}</b-td>
+              </b-tr>
+            </b-table-simple>
+          </b-col>
+          <b-col
+            cols="8"
+            offset="2"
+          >
+            <b-card>
+              <b-card-body v-if="publication.abstract">
+                <b-card-title>Abstract</b-card-title>
+                <b-card-text class="text-left">
+                  {{ publication.abstract }}
+                </b-card-text>
+              </b-card-body>
+              <b-card-body v-if="publication.keywords">
+                <b-card-title>Keywords</b-card-title>
+                <b-card-text>
+                  <span
+                    v-for="keyword in publication.keywords"
+                    :key="keyword"
+                    class="d-inline-block bg-info p-2 mx-2 text-light rounded"
+                  >
+                    {{ keyword }}
+                  </span>
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <router-link :to="{ name: 'publications' }">
+            Back to list
+          </router-link>
+        </div>
       </b-col>
     </template>
   </BaseView>
@@ -50,14 +100,16 @@ export default {
       if (!this.publication.authors) {
         return '';
       }
-      const fullNames = [];
-      this.publication.authors.forEach((cur) => {
-        fullNames.push(`${cur.last_name}, ${cur.first_name}`);
-      });
-      return fullNames.join('; ');
+      return this.publication.authors
+        .map((el) => (el.first_name ? `${el.first_name} ${el.last_name}` : el.last_name))
+        .join(', ')
+      ;
     },
     added() {
       return new Date(this.publication.created).toLocaleString();
+    },
+    type() {
+      return this.publication.type.split('_').map((el) => el[0].toUpperCase() + el.slice(1)).join(' ');
     },
   },
 };
