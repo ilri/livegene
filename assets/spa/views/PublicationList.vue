349838ad78c1a661bcf78b93242d0460422bf19a
Cezar Pendarovski <cezarpendarovski@web.de>
2021-10-10 21:57:07 +0100

    Add wordcloud

--- assets/spa/views/PublicationList.vue
+++ assets/spa/views/PublicationList.vue
@@ -14,6 +14,16 @@
         lg="10"
         class="px-0"
       >
+        <div>
+          <cloud
+            :data="words"
+            :width="cloudWidth"
+            :height="cloudHeight"
+            :rotate="rotate"
+            :font-size-mapper="fontSizeMapper"
+            :on-word-click="onWordClick"
+          />
+        </div>
         <b-pagination
           v-model="currentPage"
           :total-rows="publications.length"
@@ -42,12 +52,14 @@
 </template>
 
 <script>
+import Cloud from 'vue-d3-cloud';
 import { mapState } from 'vuex';
 import BaseView from '../components/BaseView';
 
 export default {
   name: 'MendeleyPublications',
   components: {
+    Cloud,
     BaseView,
   },
   data() {
@@ -99,11 +111,50 @@ export default {
           sortable: true,
         },
       ],
+      fontSizeMapper: (word) => Math.log2(word.value) * 5,
+      onWordClick: (word) => { console.log(word); },
+      // use bitwise NOT operator to generate a number between -2 and 2
+      // in order to create any of the following rotation angles:
+      // -90, -45, 0, 45, 90
+      // eslint-disable-next-line no-bitwise
+      rotate: () => (~~(Math.random() * 5) - 2) * 45,
     };
   },
-  computed: mapState({
-    publications: (state) => state.publication.publications,
-  }),
+  computed: {
+    ...mapState({
+      publications: (state) => state.publication.publications,
+    }),
+    words() {
+      const text = this.publications
+        // concatenate title and abstract for each publication
+        .map((el) => (el.abstract ? `${el.title} ${el.abstract}` : el.title))
+        // join everything together in one big string
+        .join(' ')
+        // clean from all non-letter characters, but keep white-space
+        .replace(/[^a-zA-Z\s]/g, ' ')
+        // make everything lowercase
+        .toLowerCase()
+        // create an array by splitting all words using white-space as separator
+        .split(/\s+/)
+        // remove all words with less than 3 letters
+        .filter((el) => el.length > 3)
+        // count the occurrences of all words
+        .reduce((acc, cur) => {
+          acc[cur] = (acc[cur] || 0) + 1;
+          return acc;
+        }, Object.create(null))
+      ;
+      const words = Object.keys(text).map((el) => ({ text: el, value: text[el] }));
+      console.dir(words);
+      return words;
+    },
+    cloudWidth() {
+      return window.innerWidth >= 992 ? (window.innerWidth / 12) * 10 : window.innerWidth;
+    },
+    cloudHeight() {
+      return this.cloudWidth / 4;
+    },
+  },
 };
 </script>
 
