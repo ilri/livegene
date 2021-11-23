661294edbc4471a9125b06e142bac3c7b3ae555c
Cezar Pendarovski <cezarpendarovski@web.de>
2021-11-23 02:57:31 +0000

    Fix Vuex store in unit tests for BaseView

--- assets/spa/tests/unit/components/BaseView.spec.js
+++ assets/spa/tests/unit/components/BaseView.spec.js
@@ -1,10 +1,13 @@
 import { mount, createLocalVue } from '@vue/test-utils';
-import BootstrapVue, { BRow } from 'bootstrap-vue';
+import Vuex from 'vuex';
+import BootstrapVue from 'bootstrap-vue';
 import NProgress from 'nprogress';
 import BaseView from '../../../components/BaseView';
+import ErrorModule from '../../../store/modules/error';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
+localVue.use(Vuex);
 
 // Cezar: trying to add instance property to localVue, to mimic the
 // Vue instance property $loadingStatus
@@ -19,35 +22,62 @@ Object.defineProperty(localVue.prototype, '$loadingStatus', {
 });
 
 describe('BaseView.vue', () => {
+  let actions;
+  let state;
+  let store;
+
+  beforeEach(() => {
+    state = {
+      errors: [],
+    };
+    actions = {
+      addAction: jest.fn(),
+    };
+    store = new Vuex.Store({
+      modules: {
+        error: {
+          state,
+          actions,
+          getters: ErrorModule.getters,
+          namespaced: true,
+        },
+      },
+    });
+  });
+
   test('display main area when there are no errors', () => {
     const wrapper = mount(BaseView, {
       localVue,
-      computed: {
-        errors: () => [],
+      store,
+      propsData: {
+        module: 'projects',
       },
     });
-    const row = wrapper.find(BRow);
+    const row = wrapper.find('div.row.text-center.pb-5');
     expect(row.exists()).toBe(true);
     expect(row.classes('row')).toBe(true);
     expect(row.classes('text-center')).toBe(true);
     expect(row.classes('show')).toBe(true);
+    expect(row.attributes('style')).toBe(undefined);
   });
 
   test('do not display main area when there are errors', () => {
+    store.state.error.errors = [
+      {
+        id: 1,
+        message: 'Error',
+        module: 'projects',
+      },
+    ];
     const wrapper = mount(BaseView, {
       localVue,
-      computed: {
-        errors: () => [
-          {
-            id: 1,
-            message: 'This is an error',
-            status: 404,
-            statusText: 'Not found',
-          },
-        ],
+      store,
+      propsData: {
+        module: 'projects',
       },
     });
-    const row = wrapper.find(BRow);
-    expect(row.exists()).toBe(false);
+    const row = wrapper.find('div.row.text-center.pb-5');
+    expect(row.exists()).toBe(true);
+    expect(row.attributes('style')).toEqual('display: none;');
   });
 });
