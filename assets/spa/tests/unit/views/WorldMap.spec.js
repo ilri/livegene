4706b82ac772d21f66621c14875f4b1169fe0ca4
Cezar Pendarovski <cezarpendarovski@web.de>
2021-12-08 09:40:55 +0000

    Refactor unit tests for WorldMap

--- assets/spa/tests/unit/views/WorldMap.spec.js
+++ assets/spa/tests/unit/views/WorldMap.spec.js
@@ -2,131 +2,141 @@ import { createLocalVue, mount, shallowMount } from '@vue/test-utils';
 import BootstrapVue from 'bootstrap-vue';
 import Vuex from 'vuex';
 import WorldMap from '../../../views/WorldMap';
+import ErrorModule from '../../../store/modules/error';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 localVue.use(Vuex);
 
 describe('WorldMap.vue', () => {
-  let state;
+  let projectState;
+  let errorState;
   let store;
 
-  state = {
-    projects: [
-      {
-        id: 1,
-        fullName: 'Project_1',
-        startDate: '2021-01-01T00:00:00+00:00',
-        endDate: '2022-01-01T00:00:00+00:00',
-        team: 'Team_1',
-        donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-        partnerships: [],
-        countryRoles: [],
-      },
-      {
-        id: 2,
-        fullName: 'Project_2',
-        startDate: '2021-01-01T00:00:00+00:00',
-        endDate: '2022-01-01T00:00:00+00:00',
-        team: 'Team_1',
-        donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-        partnerships: [],
-        countryRoles: [],
-      },
-      {
-        id: 3,
-        fullName: 'Project_3',
-        startDate: '2021-01-01T00:00:00+00:00',
-        endDate: '2022-01-01T00:00:00+00:00',
-        team: 'Team_2',
-        donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-        partnerships: [],
-        countryRoles: [],
-      },
-      {
-        id: 4,
-        fullName: 'Project_4',
-        startDate: '2021-01-01T00:00:00+00:00',
-        endDate: '2022-01-01T00:00:00+00:00',
-        team: 'Team_2',
-        donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-        partnerships: [],
-        countryRoles: [],
-      },
-      {
-        id: 5,
-        fullName: 'Project_5',
-        startDate: '2021-01-01T00:00:00+00:00',
-        endDate: '2022-01-01T00:00:00+00:00',
-        team: 'Team_2',
-        donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-        partnerships: [],
-        countryRoles: [],
-      },
-    ],
-    projectsGroupedByTeam: new Map(
-      [
-        ['Team_1',
-          [
-            {
-              id: 1,
-              fullName: 'Project_1',
-              startDate: '2021-01-01T00:00:00+00:00',
-              endDate: '2022-01-01T00:00:00+00:00',
-              donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-              partnerships: [],
-              countryRoles: [],
-            },
-            {
-              id: 2,
-              fullName: 'Project_2',
-              startDate: '2021-01-01T00:00:00+00:00',
-              endDate: '2022-01-01T00:00:00+00:00',
-              donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-              partnerships: [],
-              countryRoles: [],
-            },
-          ],
-        ],
-        ['Team_2',
-          [
-            {
-              id: 3,
-              fullName: 'Project_3',
-              startDate: '2021-01-01T00:00:00+00:00',
-              endDate: '2022-01-01T00:00:00+00:00',
-              donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-              partnerships: [],
-              countryRoles: [],
-            },
-            {
-              id: 4,
-              fullName: 'Project_4',
-              startDate: '2021-01-01T00:00:00+00:00',
-              endDate: '2022-01-01T00:00:00+00:00',
-              donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-              partnerships: [],
-              countryRoles: [],
-            },
-            {
-              id: 5,
-              fullName: 'Project_5',
-              startDate: '2021-01-01T00:00:00+00:00',
-              endDate: '2022-01-01T00:00:00+00:00',
-              donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
-              partnerships: [],
-              countryRoles: [],
-            },
-          ],
-        ],
-      ],
-    ),
-  };
   beforeEach(() => {
+    projectState = {
+      projects: [
+        {
+          id: 1,
+          fullName: 'Project_1',
+          startDate: '2021-01-01T00:00:00+00:00',
+          endDate: '2022-01-01T00:00:00+00:00',
+          team: 'Team_1',
+          donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+          partnerships: [],
+          countryRoles: [],
+        },
+        {
+          id: 2,
+          fullName: 'Project_2',
+          startDate: '2021-01-01T00:00:00+00:00',
+          endDate: '2022-01-01T00:00:00+00:00',
+          team: 'Team_1',
+          donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+          partnerships: [],
+          countryRoles: [],
+        },
+        {
+          id: 3,
+          fullName: 'Project_3',
+          startDate: '2021-01-01T00:00:00+00:00',
+          endDate: '2022-01-01T00:00:00+00:00',
+          team: 'Team_2',
+          donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+          partnerships: [],
+          countryRoles: [],
+        },
+        {
+          id: 4,
+          fullName: 'Project_4',
+          startDate: '2021-01-01T00:00:00+00:00',
+          endDate: '2022-01-01T00:00:00+00:00',
+          team: 'Team_2',
+          donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+          partnerships: [],
+          countryRoles: [],
+        },
+        {
+          id: 5,
+          fullName: 'Project_5',
+          startDate: '2021-01-01T00:00:00+00:00',
+          endDate: '2022-01-01T00:00:00+00:00',
+          team: 'Team_2',
+          donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+          partnerships: [],
+          countryRoles: [],
+        },
+      ],
+      projectsGroupedByTeam: new Map(
+        [
+          ['Team_1',
+            [
+              {
+                id: 1,
+                fullName: 'Project_1',
+                startDate: '2021-01-01T00:00:00+00:00',
+                endDate: '2022-01-01T00:00:00+00:00',
+                donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+                partnerships: [],
+                countryRoles: [],
+              },
+              {
+                id: 2,
+                fullName: 'Project_2',
+                startDate: '2021-01-01T00:00:00+00:00',
+                endDate: '2022-01-01T00:00:00+00:00',
+                donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+                partnerships: [],
+                countryRoles: [],
+              },
+            ],
+          ],
+          ['Team_2',
+            [
+              {
+                id: 3,
+                fullName: 'Project_3',
+                startDate: '2021-01-01T00:00:00+00:00',
+                endDate: '2022-01-01T00:00:00+00:00',
+                donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+                partnerships: [],
+                countryRoles: [],
+              },
+              {
+                id: 4,
+                fullName: 'Project_4',
+                startDate: '2021-01-01T00:00:00+00:00',
+                endDate: '2022-01-01T00:00:00+00:00',
+                donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+                partnerships: [],
+                countryRoles: [],
+              },
+              {
+                id: 5,
+                fullName: 'Project_5',
+                startDate: '2021-01-01T00:00:00+00:00',
+                endDate: '2022-01-01T00:00:00+00:00',
+                donor: { id: 1, shortName: 'ACME', fullName: 'A Company that Makes Everything' },
+                partnerships: [],
+                countryRoles: [],
+              },
+            ],
+          ],
+        ],
+      ),
+    };
+    errorState = {
+      errors: [],
+    };
     store = new Vuex.Store({
       modules: {
         project: {
-          state,
+          state: projectState,
+          namespaced: true,
+        },
+        error: {
+          state: errorState,
+          getters: ErrorModule.getters,
           namespaced: true,
         },
       },
@@ -157,7 +167,7 @@ describe('WorldMap.vue', () => {
   });
 
   test('calls selectTeam() method when user selects (clicks on) a team', () => {
-    const wrapper = shallowMount(WorldMap, {
+    const wrapper = mount(WorldMap, {
       localVue,
       store,
       data() {
@@ -166,7 +176,6 @@ describe('WorldMap.vue', () => {
         };
       },
     });
-    console.log(wrapper.html());
     jest.spyOn(wrapper.vm, 'selectTeam');
     const teamLinks = wrapper.findAll('li.team > span');
     teamLinks.wrappers.forEach((teamLink) => {
@@ -176,7 +185,7 @@ describe('WorldMap.vue', () => {
   });
 
   test('calls selectProject() when user selects (clicks on) a project', () => {
-    const wrapper = shallowMount(WorldMap, {
+    const wrapper = mount(WorldMap, {
       localVue,
       store,
     });
