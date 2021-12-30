46ecc838c4ca2c5bd322df8f76b2e8d0becb5079
Cezar Pendarovski <cezarpendarovski@web.de>
2021-12-30 15:16:49 +0000

    Update command

--- src/Command/MendeleyNewPublicationsNotifierCommand.php
+++ src/Command/MendeleyNewPublicationsNotifierCommand.php
@@ -2,26 +2,44 @@
 
 namespace App\Command;
 
+use App\Exception\CacheItemNotFoundException;
+use App\Mailer\NewPublicationsMailer;
+use App\Repository\Mendeley\PublicationRepository;
 use App\Repository\StaffMemberRepository;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Twig\Error\{
+    LoaderError,
+    RuntimeError,
+    SyntaxError,
+};
 
 class MendeleyNewPublicationsNotifierCommand extends Command
 {
     private const HOME_PROGRAM = 'LiveGene';
+    private const PERIOD = '-1 week';
 
     protected static $defaultName = 'app:mendeley:new-publications-notifier';
     protected static string $defaultDescription = 'Notify staff members from Home Program LiveGene about new publications';
 
     private StaffMemberRepository $staffMemberRepository;
+    private PublicationRepository $publicationRepository;
+    private NewPublicationsMailer $newPublicationsMailer;
 
-    public function __construct(StaffMemberRepository $staffMemberRepository, string $name = null)
-    {
+    public function __construct(
+        StaffMemberRepository $staffMemberRepository,
+        PublicationRepository $publicationRepository,
+        NewPublicationsMailer $newPublicationsMailer,
+        string $name = null
+    ) {
         parent::__construct($name);
 
         $this->staffMemberRepository = $staffMemberRepository;
+        $this->publicationRepository = $publicationRepository;
+        $this->newPublicationsMailer = $newPublicationsMailer;
     }
 
     protected function configure(): void
@@ -37,15 +55,40 @@ class MendeleyNewPublicationsNotifierCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $liveGeneStaff = $this->staffMemberRepository
-            ->findByHomeProgram(self::HOME_PROGRAM)
-        ;
-
-        $io->progressStart(count($liveGeneStaff));
-        foreach ($liveGeneStaff as $staffMember) {
-            $io->progressAdvance();
+        $isoDate = (new \DateTime(self::PERIOD))->format('c');
+        try {
+            $newPublications = $this->publicationRepository->getPublicationsModifiedSince($isoDate);
+            $publications = array_map(
+              function($publication) {
+                  $publication['mode'] = substr($publication['created'], 0, 10) == substr($publication['last_modified'], 0, 10) ? 'NEW' : 'UPDATED';
+                  return $publication;
+              }, $newPublications
+            );
+        } catch  (GuzzleException | CacheItemNotFoundException $e) {
+            $io->writeln([sprintf('<error>%s</error>', $e->getMessage())]);
+            return 1;
         }
-        $io->progressFinish();
+
+        if ($publications) {
+            $liveGeneStaff = $this->staffMemberRepository
+                ->findByHomeProgram(self::HOME_PROGRAM)
+            ;
+            $emailAddresses = array_map(
+                function ($staffMember) {
+                    return $staffMember->getEmail();
+                }, $liveGeneStaff
+            );
+
+            try {
+                $this->newPublicationsMailer->sendNewPublicationsNotificationMessage(
+                    $emailAddresses,
+                    $publications,
+                );
+            } catch (LoaderError | RuntimeError | SyntaxError $e) {
+                $io->writeln([sprintf('<error>%s</error>', $e->getMessage())]);
+                return 1;
+            }
+        }
 
         return 0;
     }
